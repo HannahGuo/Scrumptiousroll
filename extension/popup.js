ebae0ed1364fcfcb1e1b630c80f818c31ceecb01
WillQi <williamqipizza@gmail.com>
2021-01-16 13:31:12 -0700

    initial prediction code, needs model still

--- extension/popup.js
+++ extension/popup.js
@@ -5,5 +5,14 @@ foundFoodButton.onclick = function (element) {
     alert(image);
     let newImg = `<img src="${image}">`
     document.getElementById("labelledImage").innerHTML = newImg;
+
+    // Predict the food
+    const imageElement = document.querySelector('#labelledImage > img');
+    imageElement.onload = () => { // Tensorflow doesn't like not seeing a width and height attribute on the element, so we have to set it ourselves before classifying it.
+      imageElement.setAttribute('width', imageElement.width);
+      imageElement.setAttribute('height', imageElement.height);
+      getFoodPredictionsForImage(imageElement).then(predictions => alert(JSON.stringify(predictions)));
+    };
+
   });
 };
